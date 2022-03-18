ad8d595b43249a05fcb6d98919358305126f8236
colton <coltonnwalker@gmail.com>
2022-03-18 15:55:08 -0400

    fixed 5_5 with forcing list values to be integers

--- 5_5.py
+++ 5_5.py
@@ -6,11 +6,37 @@ while True :
         break
 
     try :
-        int(n)
+        intn = int(n)
     except :
         print("Invalid Type")
         continue
-    humanList.append(n)
-print("Largest number is:", max(humanList))
-print("Smallest number is:", min(humanList))
-        
+    humanList.append(intn)
+
+def listMean(x) :
+    listSum = 0
+    counter = 0
+    int(listSum)
+    for i in x :
+        int(i)
+        listSum = listSum + i
+        counter = counter + 1
+    return(listSum / counter)
+
+def largeNumber(x) :
+    num = None
+    for i in x :
+        if num == None or num < i :
+         num = i
+    return num
+
+def smallNumber(x) :
+    num = None
+    for i in x :
+        if num == None or num > i :
+            num = i
+    return num
+
+
+print("Largest:", largeNumber(humanList), "Smallest:", smallNumber(humanList) , "Average:", listMean(humanList))
+    
+
